160a156d615eac2279874169c856965548df4c00
RisingBeastVector <saiayushmanp45@gmail.com>
2025-06-10 23:16:18 +0530

    Hello World mssg

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,3 +1,8 @@
 console.log("Hello World!");
 
 // Used to print messages or values to the browser's console (or Node.js terminal).
+
+// JavaScript mein semicolon ka use optional hai !!
+/* Lekin, best practice hai ki semicolon ka use karein,
+   kyunki yeh code ko zyada readable banata hai aur errors se bachata hai.  
+*/                                        
