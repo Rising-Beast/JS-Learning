ae3f8395ee8f1f80bcd07d49393f9ba0b21659be
RisingBeastVector <saiayushmanp45@gmail.com>
2025-06-10 18:50:05 +0530

    Hello World mssg

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,3 +1,3 @@
 console.log("Hello World!");
 
-// Used to print messages or values to the browser's console (or Node.js terminal)
+// Used to print messages or values to the browser's console (or Node.js terminal).
