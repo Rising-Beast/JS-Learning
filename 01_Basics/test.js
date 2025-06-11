27c6004848c3c83cbfb31dc0980ef26c49df54d2
RisingBeastVector <saiayushmanp45@gmail.com>
2025-06-11 19:38:58 +0530

    Hello World mssg

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -2,7 +2,11 @@ console.log("Hello World!");
 
 // Used to print messages or values to the browser's console (or Node.js terminal).
 
+/* Prettier extension automatically semicolons add kar deta hai,
+   Isliye aapko manually semicolons add karne ki jarurat nahi hai.
+*/
+
 // JavaScript mein semicolon ka use optional hai !!
 /* Lekin, best practice hai ki semicolon ka use karein,
    kyunki yeh code ko zyada readable banata hai aur errors se bachata hai.  
-*/                                        
+*/ 
