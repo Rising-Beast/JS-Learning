ba3f570d299e34ecb68a54a1e6d3f56b538d8692
RisingBeastVector <saiayushmanp45@gmail.com>
2025-06-26 01:13:44 +0530

    Strings

--- 01_Basics/08_strings.js
+++ 01_Basics/08_strings.js
@@ -28,6 +28,7 @@ console.log(`${name} has ${repoCount} repositories.`); // String Interpolation
         // new Object() se object create hota hai.
 */
 
+
 const gameName = new String("Call of Duty");
 /* new String() se string object create hota hai, lekin yeh jyada use nahi hoti hai.
    Iska use karne se string primitive type nahi hota, balki ek String object banta hai.
@@ -41,12 +42,12 @@ const gameName = new String("Call of Duty");
    Isliye, iski length bhi hoti hai, aur yeh String.prototype se inherit hota hai.
 */
 
-console.log(gameName); // [String: 'Call of Duty']
+console.log(gameName);        // [String: 'Call of Duty']
 console.log(typeof gameName); // object (String object)
 
 
 // To access each character of the string, we can use indexing:
-console.log(name[15]); // "d"
+console.log(name[15]);    // "d"
 console.log(gameName[3]); // "l"
 /* String indexing zero-based hoti hai, matlab pehla character index 0 par hota hai.
    Backwards indexing bhi hoti hai, matlab last character ko -1 se access kar sakte hain:
@@ -65,26 +66,32 @@ console.log(gameName.__proto__); // String.prototype // Output: {} (Empty object
    for string manipulation.
 */
 
-console.log(gameName.length); // 12
+
+console.log(gameName.length);        // 12
 console.log(gameName.toUpperCase()); // "CALL OF DUTY"
-console.log(gameName.charAt(2)); // "l"
-console.log(gameName.indexOf('t')); // 10
+console.log(gameName.charAt(2));     // "l"
+console.log(gameName.indexOf('t'));  // 10
 
 const newString = gameName.substring(0, 6); // "Call o" - yeh 0 se 6 tak ke characters ko return karega.
 console.log(newString); 
 
+
 const anotherString = gameName.slice(-7, 10); // "of Du"
 console.log(anotherString);
+
 
 const newStringOne = "   Hello World!  ";
 console.log(newStringOne);
 
 console.log(newStringOne.trim()); // trim() method se string ke starting aur ending spaces remove ho jate hain.
 
+
 const url = "https://sai.com/sai%20ayushman"
 
 console.log(url.replace("%20", "-")); // replace() method se string mein kisi specific substring ko replace kar sakte hain.
 
+
 console.log(url.includes("sundar")); // includes() method se check kar sakte hain ki string mein koi specific substring hai ya nahi.
+
 
 console.log(gameName.split(" ")); // split() method se string ko kisi specific delimiter par tod sakte hain.
