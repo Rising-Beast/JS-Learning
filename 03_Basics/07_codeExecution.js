60ff4f81facd2e946bf46348d938c3b3e749b5ea
Rising-Beast <saiayushmanp45@gmail.com>
2025-06-30 00:23:11 +0530

    Execution context example

---
+++ 03_Basics/07_codeExecution.js
@@ -0,0 +1,10 @@
+// Code snippet used in 07_codeExecution.excalidraw :
+
+let val1 = 10;
+let val2 = 20;
+function addNum(num1, num2) {
+    let total = num1 + num2;
+    return total;
+}
+let result1 = addNum(val1, val2);
+let result2 = addNum(5, 10);
